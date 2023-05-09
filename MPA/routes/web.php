68942947dbc4d6779b25d2a6c8984f15c8241de2
LuukS <Luukschouten01@gmail.com>
2023-05-09 12:20:45 +0200

    allow only logged in users to use app

--- MPA/routes/web.php
+++ MPA/routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Songcontroller;
 use App\Http\Controllers\Playlistcontroller;
 use App\Http\Controllers\Sessioncontroller;
+use App\Http\middleware;
 
 use App\Models\Song;
 use App\Models\Playlist;
@@ -34,81 +35,87 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/songsOverview', [Songcontroller::class, 'read']);
+Route::middleware(['auth'])->group(function () {
 
-Route::get('/song/{song}', function($id){
-    $song = Song::findOrFail($id);
-    $playlist = Playlist::all();
-    return view('song')
-        ->with('song', $song)
-        ->with('playlist', $playlist);
-});
+    Route::get('/songsOverview', [Songcontroller::class, 'read']);
 
-Route::get('createSong', function(){
-    return view('createSong');
-});
-
-Route::post('createSong', [Songcontroller::class, 'create']);
-
-Route::get('/editSong/{song}' , function($id){
-    $song = Song::findOrFail($id);
-    return view('editSong')
-        ->with('song', $song);
-});
-
-Route::post('/editSong/{song}', [Songcontroller::class, 'edit']);
-
-Route::get('/deleteSong/{song}', function($id){
-    $song = Song::findOrFail($id);
-    Song::where('id', $id)->delete();
-    return redirect()->back();
-});
-
-Route::get('/playlistsOverview', [Playlistcontroller::class, 'read']);
-
-Route::get('/editPlaylist/{playlist}' , function($id){
-    $playlist = Playlist::findOrFail($id);
-    
-    return view('editPlaylist')
-        ->with('playlist', $playlist);
+    Route::get('/song/{song}', function($id){
+        $song = Song::findOrFail($id);
+        $playlist = Playlist::all();
+        return view('song')
+            ->with('song', $song)
+            ->with('playlist', $playlist);
     });
-
-Route::post('/editPlaylist/{playlist}', [Playlistcontroller::class, 'edit']);
-
-Route::post('/addToPLaylist/{song}', [Songcontroller::class, 'addToPlaylist']);
-
-Route::get('/playlist/{playlist}', function($id){
-    $playlist = Playlist::findOrFail($id);
-    $songs = Song::where("playlist_id", $id)->get();
-    $totalTime = DB::select("SELECT  SEC_TO_TIME( SUM( TIME_TO_SEC( `length` ) ) ) AS timeSum FROM songs where playlist_id = '$id'");
-
-    return view('playlist')
-        ->with('playlist', $playlist)
-        ->with('songs', $songs)
-        ->with('totalTime', $totalTime);
+    
+    Route::get('createSong', function(){
+        return view('createSong');
+    });
+    
+    Route::post('createSong', [Songcontroller::class, 'create']);
+    
+    Route::get('/editSong/{song}' , function($id){
+        $song = Song::findOrFail($id);
+        return view('editSong')
+            ->with('song', $song);
+    });
+    
+    Route::post('/editSong/{song}', [Songcontroller::class, 'edit']);
+    
+    Route::get('/deleteSong/{song}', function($id){
+        $song = Song::findOrFail($id);
+        Song::where('id', $id)->delete();
+        return redirect()->back();
+    });
+    
+    Route::get('/playlistsOverview', [Playlistcontroller::class, 'read']);
+    
+    Route::get('/editPlaylist/{playlist}' , function($id){
+        $playlist = Playlist::findOrFail($id);
+        
+        return view('editPlaylist')
+            ->with('playlist', $playlist);
+        });
+    
+    Route::post('/editPlaylist/{playlist}', [Playlistcontroller::class, 'edit']);
+    
+    Route::post('/addToPLaylist/{song}', [Songcontroller::class, 'addToPlaylist']);
+    
+    Route::get('/playlist/{playlist}', function($id){
+        $playlist = Playlist::findOrFail($id);
+        $songs = Song::where("playlist_id", $id)->get();
+        $totalTime = DB::select("SELECT  SEC_TO_TIME( SUM( TIME_TO_SEC( `length` ) ) ) AS timeSum FROM songs where playlist_id = '$id'");
+    
+        return view('playlist')
+            ->with('playlist', $playlist)
+            ->with('songs', $songs)
+            ->with('totalTime', $totalTime);
+    });
+    
+    Route::get('/createPlaylist', function() {
+        return view('createPlaylist');
+    });
+    
+    Route::post('/createPlaylist', [Playlistcontroller::class, 'create']);
+    
+    Route::get('/deletePlaylist/{playlist}', function($id){
+        $playlist = Playlist::findOrFail($id);
+        Playlist::where('id', $id)->delete();
+        return redirect()->back();
+    });
+    
+    Route::get('/deletefromPlaylist/{song}', function($id){
+        $song = Song::findOrFail($id);
+        Song::where('id', $id)->update(["playlist_id" => 0]);;
+        return redirect()->back();
+    });
+    
+    Route::get('/addToQueue/{song}', [Sessioncontroller::class, 'storeSession', ]);
+    
+    Route::get('/Queue', [Sessioncontroller::class, 'showSession']);
+    
 });
 
-Route::get('/createPlaylist', function() {
-    return view('createPlaylist');
-});
 
-Route::post('/createPlaylist', [Playlistcontroller::class, 'create']);
-
-Route::get('/deletePlaylist/{playlist}', function($id){
-    $playlist = Playlist::findOrFail($id);
-    Playlist::where('id', $id)->delete();
-    return redirect()->back();
-});
-
-Route::get('/deletefromPlaylist/{song}', function($id){
-    $song = Song::findOrFail($id);
-    Song::where('id', $id)->update(["playlist_id" => 0]);;
-    return redirect()->back();
-});
-
-Route::get('/addToQueue/{song}', [Sessioncontroller::class, 'storeSession', ]);
-
-Route::get('/Queue', [Sessioncontroller::class, 'showSession']);
 
 
 require __DIR__.'/auth.php';
